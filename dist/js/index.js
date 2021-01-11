587cfca9d8c0d15d435ea6c43c0fec68491d350e
web-wj <451959831@qq/com>
2021-01-11 09:25:01 +0800

    over

--- dist/js/index.js
+++
@@ -1,1 +0,0 @@
-const arr=[0,1,2,3,4],num=0;for(var i=0;i<arr.length;i++)num+=arr[i];
--- gulpfile.js
+++ gulpfile.js
@@ -8,35 +8,61 @@ const uglify = require('gulp-uglify');
 const imageMin = require('gulp-imagemin');
 const connect = require('gulp-connect');
 
-const floder = {
+const folder = {
     src: 'src/',
     dist: 'dist/'
 }
 
 function html() {
-    return src(floder.src + 'html/*')
+    return src(folder.src + 'html/*')
         .pipe(htmlClean())
-        .pipe(dest(floder.dist + 'html/'));
+        .pipe(dest(folder.dist + 'html/'))
+        .pipe(connect.reload());
 }
 
 function css() {
-    return src(floder.src + 'css/*')
+    return src(folder.src + 'css/*')
         .pipe(less())
         .pipe(cleanCss())
-        .pipe(dest(floder.dist + 'css/'));
+        .pipe(dest(folder.dist + 'css/'))
+        .pipe(connect.reload());
 }
 
 function js() {
-    return src(floder.src + 'js/*')
+    return src(folder.src + 'js/*')
         .pipe(stripDebug())
         .pipe(uglify())
-        .pipe(dest(floder.dist + 'js/'));
+        .pipe(dest(folder.dist + 'js/'))
+        .pipe(connect.reload());
 }
 
 function image() {
-    return src(floder.src + 'images/*')
+    return src(folder.src + 'images/*')
         .pipe(imageMin())
-        .pipe(dest(floder.dist + 'images/'));
+        .pipe(dest(folder.dist + 'images/'));
 }
 
-exports.default = series(html, css, js, image);
+function server(cb) {
+    connect.server({
+        port: '1573',
+        livereload: true, //自动刷新
+    });
+
+    cb();
+}
+
+watch(folder.src + 'html/*', function(cb) {
+    html();
+    cb();
+})
+
+watch(folder.src + 'css/*', function(cb) {
+    css();
+    cb();
+});
+watch(folder.src + 'js/*', function(cb) {
+    js();
+    cb();
+});
+
+exports.default = series(html, css, js, image, server)
