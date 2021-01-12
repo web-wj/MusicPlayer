f0742f2795ac3888231c8cabbdac25f51152765c
web-wj <451959831@qq/com>
2021-01-12 16:19:07 +0800

    base-make

---
+++ src/js/progress.js
@@ -0,0 +1,26 @@
+(function() {
+    //进度条的对象
+    function Progress() {
+        this.
+    }
+    Progress.prototype = {
+
+    };
+    //拖拽对象
+    function Drag() {
+
+    }
+    Drag.prototype = {
+
+    }
+
+    function instanceProgress() {
+        return new Progress();
+    }
+
+    function instanceDrag() {
+        return new Drag();
+    }
+
+
+}(window.player || (window.player = {})))
